0a005fb1ed4198fdcf55d885e59c8bb697087e7e
Tyler Johnston <tylerjohnst@gmail.com>
2013-11-09 14:20:39 -0500

    Tweaks

--- Rakefile
+++ Rakefile
@@ -5,7 +5,7 @@ require_relative 'lib/compiler'
 task :default => [:build]
 
 task :build do
-  cd = File.dirname(__FILE__)
-  I18n.load_path = Dir[File.join(cd, 'locales', '*.yml').to_s]
-  Compiler.build! File.dirname(__FILE__)
+  current_directory = File.dirname(__FILE__)
+  I18n.load_path = Dir[File.join(current_directory, 'locales', '*.yml').to_s]
+  Compiler.build! current_directory
 end
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,10 @@
 $('.carousel').carousel();
-$('.links a').tooltip()
+$('.links a').tooltip();
 
 $('ul.nav').on('click', 'a', function(event) {
-  var $a = $(event.target);
-  var anchor = $a.attr('href');
+  var anchor = $(event.target).attr('href');
   var $div = $(anchor);
-  var top = $div.position().top;
-
-  top = ( anchor === '#about' ? top - 100 : top - 50 );
-
+  var top = $div.position().top - 100;
   $('body').animate({scrollTop: top}, 500);
-  event.preventDefault()
+  event.preventDefault();
 });
